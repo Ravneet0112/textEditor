5268bedcc4000df017d533e79b53ac6db3bf61c0
Ravneet Kaur Sammewali <ravneet.sammewali01@gmail.com>
2023-03-06 17:35:10 -0500

    added readme file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,13 +13,13 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
+export const putDb = async ( content) => {
   console.log('PUT to database');
   const db = await openDB('jate', 1);
 
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put( {id: id, value: content});
+  const request = store.put( {id: 1, value: content});
 
   const result = await request;
   console.log('🚀 - data saved to the database', result);
@@ -33,11 +33,19 @@ export const getDb = async () => {
 
   const tx = db.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
+  /*
   const request = store.getAll();
 
   const result = await request;
   console.log('result.value', result);
   return result;
+  */
+  const request = store.get(1);
+
+  const result = await request;
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - no data in the database');
 
 };
 
